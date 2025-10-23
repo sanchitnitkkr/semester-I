c90ba0531e11cc74e35e9fc58aadbe92f1ace9b2
Sanchit Tewari <sanchitnitkkr@gmail.com>
2025-10-23 14:36:08 +0530

    23 Oct

--- semester-I/c/experiment_41.c
+++ semester-I/c/experiment_41.c
@@ -7,6 +7,26 @@ typedef struct
     float imaginary;
 } Complex;
 
+Complex addComplex(Complex num1, Complex num2)
+{
+    Complex result;
+    float sumReal = num1.real + num2.real;
+    float sumImaginary = num1.imaginary + num2.imaginary;
+    result.real = sumReal;
+    result.imaginary = sumImaginary;
+    return result;
+}
+
+Complex subComplex(Complex num1, Complex num2)
+{
+    Complex result;
+    float subReal = num1.real - num2.real;
+    float subImaginary = num1.imaginary - num2.imaginary;
+    result.real = subReal;
+    result.imaginary = subImaginary;
+    return result;
+}
+
 int main()
 {
     Complex num1;
@@ -27,19 +47,15 @@ int main()
     printf("Complex number 1 => %g + %gi\n", num1.real, num1.imaginary);
     printf("Complex number 2 => %g + %gi\n", num1.real, num1.imaginary);
 
-
     printf("The required operations's result are as follows:\n");
 
-    float sumReal = num1.real + num2.real;
-    float sumImaginary = num1.imaginary + num2.imaginary;
-
-    float differenceReal = num1.real - num2.real;
-    float differenceImaginary = num1.imaginary - num2.imaginary;
+    Complex complexSum = addComplex(num1, num2);
+    Complex complexDiff = subComplex(num1, num2);
 
     printf("ADDITION\n");
-    printf("(%g + %gi) +  (%g + %gi) = (%g + %gi)\n", num1.real, num1.imaginary, num2.real, num2.imaginary, sumReal, sumImaginary);
+    printf("(%g + %gi) +  (%g + %gi) = (%g + %gi)\n", num1.real, num1.imaginary, num2.real, num2.imaginary, complexSum.real, complexSum.imaginary);
     printf("SUBTRACTION\n");
-    printf("(%g + %gi) + (%g + %gi) = (%g + %gi)\n", num1.real, num1.imaginary, num2.real, num2.imaginary, differenceReal, differenceImaginary);
+    printf("(%g + %gi) + (%g + %gi) = (%g + %gi)\n", num1.real, num1.imaginary, num2.real, num2.imaginary, complexDiff.real, complexDiff.imaginary);
 
     return 0;
 }
