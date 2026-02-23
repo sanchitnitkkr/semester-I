648081bd4e11c66e279c90652de5cd315a4dc72f
Sanchit Tewari <sanchitnitkkr@gmail.com>
2026-02-23 10:08:26 +0530

    23 feb

--- semester-II/java/labFiles/W4_Q5.java
+++ semester-II/java/labFiles/W4_Q5.java
@@ -1,8 +1,46 @@
 package labFiles;
 
+import java.util.UUID;
+
+class AccountException extends RuntimeException {
+    AccountException(String message) {
+        super(message);
+    }
+}
+
+class DepositCheckException extends AccountException {
+    DepositCheckException(String message) {
+        super(message);
+    }
+}
+
+class WithdrawCheckException extends AccountException {
+    WithdrawCheckException(String message) {
+        super(message);
+    }
+}
+
+class AddCompoundInterestException extends AccountException {
+    AddCompoundInterestException(String message) {
+        super(message);
+    }
+}
+
+class DepositMoneyException extends AccountException {
+    DepositMoneyException(String message) {
+        super(message);
+    }
+}
+
+class WithdrawMoneyException extends AccountException {
+    WithdrawMoneyException(String message) {
+        super(message);
+    }
+}
+
 enum AccountType {
     SAVINGS("savings"),
-    CURRENT("curren");
+    CURRENT("current");
 
     private final String value;
 
@@ -15,10 +53,75 @@ enum AccountType {
     }
 }
 
-class BaseAccount {
+class Cheque {
+    private enum Status {
+        DEPOSITED, CANCELLED, WITHDRAWN
+    }
+
+    private UUID checkID;
+    private String issuedTo;
+    private int amount;
+    private Status status = Status.DEPOSITED;
+
+    Cheque(String issuedTo, int amount) {
+        UUID uuid = UUID.randomUUID();
+        this.checkID = uuid;
+        this.issuedTo = issuedTo;
+        this.amount = amount;
+        this.status = Status.DEPOSITED;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public UUID getCheckID() {
+        return checkID;
+    }
+
+    public String getIssuedTo() {
+        return issuedTo;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void cancelCheque(Cheque cheque) {
+        this.status = Status.CANCELLED;
+    }
+}
+
+interface CheckBookFacility {
+    Cheque depositCheque(String issuedTo, int amount) throws DepositCheckException;
+
+    void cancelCheque(Cheque cheque) throws WithdrawCheckException;
+}
+
+interface CompoundInterestFacility {
+    void addCompoundInterest() throws AddCompoundInterestException;
+}
+
+interface TransactionFacility {
+    void deposit(int amount) throws DepositMoneyException;
+
+    void withdraw(int amount) throws WithdrawMoneyException;
+}
+
+abstract class BaseAccount {
     private String customerName;
     private String accountNumber;
     private AccountType accountType;
+
+    BaseAccount(String customerName, String accountNumber, AccountType accountType) {
+        this.customerName = customerName;
+        this.accountNumber = accountNumber;
+        this.accountType = accountType;
+    }
+
+    abstract void displayBalance();
+
+    abstract void imposePenaltyOnLowBalance();
 
     public String getAccountNumber() {
         return accountNumber;
@@ -31,11 +134,25 @@ class BaseAccount {
     public String getCustomerName() {
         return customerName;
     }
+
+    protected void checkWithdrawAllowedElseThrowError(int balance, int amount) throws WithdrawMoneyException {
+        if (amount < 0) {
+            throw new WithdrawMoneyException("Invalid withdraw amount");
+        }
+
+        if (balance < amount) {
+            throw new WithdrawMoneyException("Not enough balance");
+        }
+    }
 };
 
-class CurrentAccount extends BaseAccount {
+class CurrentAccount extends BaseAccount implements CheckBookFacility, TransactionFacility {
     private int balance;
-    
+
+    CurrentAccount(String customerName, String accountNumber, AccountType accountType, int balance) {
+        super(customerName, accountNumber, accountType);
+        this.balance = balance;
+    }
 
     public int getBalance() {
         return balance;
@@ -45,23 +162,53 @@ class CurrentAccount extends BaseAccount {
         this.balance = balance;
     }
 
+    @Override
+    void imposePenaltyOnLowBalance() {
+        if (getBalance() <= 2000) {
+            setBalance(balance - 50);
+        }
+    }
+
+    @Override
     public void displayBalance() {
         System.out.println("Current balance is => " + getBalance());
     }
 
-    private boolean withdrawlAllowed() {
-        return false;
+    @Override
+    public Cheque depositCheque(String issuedToAccountNumber, int amount) {
+        return new Cheque(issuedToAccountNumber, amount);
     }
 
-    private boolean checkBookFacilityAllowed() {
-        return true;
+    @Override
+    public void cancelCheque(Cheque cheque) throws WithdrawCheckException {
+        this.cancelCheque(cheque);
     }
 
+    @Override
+    public void withdraw(int withdrawlAmount) throws WithdrawMoneyException {
+        checkWithdrawAllowedElseThrowError(getBalance(), withdrawlAmount);
+
+        int updatedAmt = getBalance() - withdrawlAmount;
+        setBalance(updatedAmt);
+
+    }
+
+    @Override
+    public void deposit(int depositAmount) throws DepositMoneyException {
+        int updatedBal = getBalance() + depositAmount;
+        setBalance(updatedBal);
+
+    }
 }
 
-class SavingsAccount extends BaseAccount {
+class SavingsAccount extends BaseAccount implements CompoundInterestFacility, TransactionFacility {
     private int balance;
-    private int interestRate;
+    private final int interestRate = 6;
+
+    SavingsAccount(String customerName, String accountNumber, AccountType accountType, int balance) {
+        super(customerName, accountNumber, accountType);
+        this.balance = balance;
+    }
 
     public int getBalance() {
         return balance;
@@ -70,36 +217,55 @@ class SavingsAccount extends BaseAccount {
     private void setBalance(int balance) {
         this.balance = balance;
     };
-
-    public void displayBalance() {
-        System.out.println("Current balance is => " + getBalance());
-    }
 
     private int computeInterest() {
         return (this.balance * this.interestRate) / 100;
     }
 
-    private void depositInterest() {
-        this.setBalance(this.getBalance() + this.computeInterest());
+    @Override
+    void imposePenaltyOnLowBalance() {
+        if (getBalance() <= 2000) {
+            setBalance(balance - 50);
+        }
     }
 
-    private boolean withdrawlAllowed() {
-        return true;
+    @Override
+    public void displayBalance() {
+        System.out.println("Current balance is => " + getBalance());
     }
 
-    int withdraw(int withdrawlAmount) {
-        if (!withdrawlAllowed()) {
-            throw new Error("Witdrawl is not allowed");
-        }
-        if (withdrawlAmount <= getBalance()) {
-            setBalance(getBalance() - withdrawlAmount);
-            return getBalance();
-        } else {
-            throw new Error("Withdrawl amount exceeds the balance");
-        }
+    @Override
+    public void addCompoundInterest() {
+        setBalance(getBalance() + this.computeInterest());
+    }
+
+    @Override
+    public void withdraw(int withdrawlAmount) {
+        checkWithdrawAllowedElseThrowError(getBalance(), withdrawlAmount);
+        setBalance(getBalance() - withdrawlAmount);
+    }
+
+    @Override
+    public void deposit(int depositAmount) throws DepositMoneyException {
+        int updatedBal = getBalance() + depositAmount;
+        setBalance(updatedBal);
+
     }
 }
 
 public class W4_Q5 {
+    public static void main(String[] args) {
+        SavingsAccount savingsAccount = new SavingsAccount("Sanchit", "172390274", AccountType.SAVINGS, 5000);
+        savingsAccount.displayBalance();
+        savingsAccount.withdraw(4000);
+        savingsAccount.displayBalance();
+        savingsAccount.deposit(2000);
+        savingsAccount.displayBalance();
+
+        savingsAccount.addCompoundInterest(); // Execute this in a cron job which runs every x days
+        savingsAccount.imposePenaltyOnLowBalance(); // Execute this in a cron job which runs every x days
+
+        savingsAccount.displayBalance();
+    }
 
 }
