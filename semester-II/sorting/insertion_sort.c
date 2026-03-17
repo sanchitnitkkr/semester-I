f07da39f0bb6419aebe9a6d34c0c56344643d8f5
Sanchit Tewari <sanchitnitkkr@gmail.com>
2026-03-17 16:00:43 +0530

    17 march

---
+++ semester-II/sorting/insertion_sort.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void printArray(int arr[], int size)
+{
+
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+void insertionSort(int arr[], int size)
+{
+    int index = 0;
+
+    for (int i = 1; i < size; i++)
+    {
+        int j = i;
+
+        while (j > 0 && arr[j] < arr[j - 1])
+        {
+            int temp = arr[j];
+            arr[j] = arr[j - 1];
+            arr[j - 1] = temp;
+            j--;
+        }
+
+        printArray(arr, size);
+    }
+}
+
+int main()
+{
+    int arr[] = {6, 3, 6, 8, 9, 1, 2, 7};
+    int size = sizeof(arr) / sizeof(int);
+
+    insertionSort(arr, size);
+
+    return 0;
+}
